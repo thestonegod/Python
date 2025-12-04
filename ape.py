3a114dd4dda57cdeaa1905b652e81126b652a801
thestonegod <elshaddaisandstone@gmail.com>
2025-12-03 20:16:11 -0800

    My own py (Pie)

---
+++ ape.py
@@ -0,0 +1,10 @@
+w=10
+if w <=15 : print("Big pie")
+person = "Joy"
+city = "Kumasi"
+age = 15
+school = "Akenteng"
+profile = person,city,age,school
+print(profile)
+print(f"My name is {person}. I am {age} years old living in {city}" 
+" and I school at {school} ") 
